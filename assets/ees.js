116b86e81fca372077704cc1e64d12b34f62b629
Nikollas Betiol <nikollasbetiol@gmail.com>
2016-04-26 16:18:56 -0400

    Added new function BACK-TO-UP

--- assets/ees.js
+++ assets/ees.js
@@ -448,4 +448,24 @@ $(document).ready(function () {
 		$('.topbar__nav-control').toggleClass('topbar__nav-control--active');
 	});
 });
+'use strict';
+
+$(document).ready(function () {
+  var offset = 220;
+  var duration = 500;
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > offset) {
+      $('.back-to-top').fadeIn(duration);
+    } else {
+      $('.back-to-top').fadeOut(duration);
+    }
+  });
+
+  $('.back-to-top').click(function (event) {
+    event.preventDefault();
+    jQuery('html, body').animate({ scrollTop: 0 }, duration);
+    return false;
+  });
+});
 "use strict";
